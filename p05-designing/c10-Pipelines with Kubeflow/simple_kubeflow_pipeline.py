8c649dc72e6a217621be38ccf8df9cd8dd09436c
wilsonify <tom.a.wilson@gmail.com>
2023-10-09 10:24:50 -0400

    update kubeflow

--- p05-designing/c10-Pipelines with Kubeflow/simple_kubeflow_pipeline.py
+++ p05-designing/c10-Pipelines with Kubeflow/simple_kubeflow_pipeline.py
@@ -1,8 +1,8 @@
-import kfp
 import pandas as pd
 from joblib import dump
 from joblib import load
 from kfp import dsl
+from kfp.deprecated.compiler import Compiler
 from kfp.deprecated.components import InputPath, OutputPath
 from kfp.deprecated.components import create_component_from_func
 from sklearn.linear_model import LinearRegression
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split
 
 
 def download_dataset(df_all_data_path: OutputPath(str)):
-    url = "https://bit.ly/3POP8CI"
+    url = "https://github.com/wilsonify/Machine-Learning-Engineering-on-AWS/raw/main/p05-designing/c10-Pipelines%20with%20Kubeflow/management_experience_and_salary.csv"
     df_all_data = pd.read_csv(url)
     df_all_data.to_csv(df_all_data_path, header=True, index=False)
 
@@ -23,31 +23,31 @@ def process_data(
     df_all_data = pd.read_csv(df_all_data_path)
     X = df_all_data['management_experience_months'].values
     y = df_all_data['monthly_salary'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-    df_training_data = pd.DataFrame({'monthly_salary': y_train, 'management_experience_months': X_train})
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+    df_training_data = pd.DataFrame({'monthly_salary': y_train, 'management_experience_months': x_train})
     df_training_data.to_csv(df_training_data_path, header=True, index=False)
-    df_test_data = pd.DataFrame({'monthly_salary': y_test, 'management_experience_months': X_test})
+    df_test_data = pd.DataFrame({'monthly_salary': y_test, 'management_experience_months': x_test})
     df_test_data.to_csv(df_test_data_path, header=True, index=False)
 
 
 def train_model(df_training_data_path: InputPath(str), model_path: OutputPath(str)):
     df_training_data = pd.read_csv(df_training_data_path)
-    print(df_training_data)
-
-    X_train = df_training_data['management_experience_months'].values
+    print(f"df_training_data.shape = {df_training_data.shape}")
+    x_train = df_training_data['management_experience_months'].values
     y_train = df_training_data['monthly_salary'].values
-
-    model = LinearRegression().fit(X_train.reshape(-1, 1), y_train)
-    print(model)
+    model = LinearRegression()
+    model.fit(x_train.reshape(-1, 1), y_train)
     dump(model, model_path)
+    print(f"model.__dict__ = {model.__dict__}")
+    print(f"model_path = {model_path}")
 
 
 def evaluate_model(model_path: InputPath(str), df_test_data_path: InputPath(str)):
     df_test_data = pd.read_csv(df_test_data_path)
-    X_test = df_test_data['management_experience_months'].values
+    x_test = df_test_data['management_experience_months'].values
     y_test = df_test_data['monthly_salary'].values
     model = load(model_path)
-    print(model.score(X_test.reshape(-1, 1), y_test))
+    print(model.score(x_test.reshape(-1, 1), y_test))
 
 
 def perform_sample_prediction(model_path: InputPath(str)):
@@ -94,4 +94,4 @@ def basic_pipeline():
     PERFORM_SAMPLE_PREDICTION.after(EVALUATE_MODEL)
 
 
-kfp.compiler.Compiler().compile(basic_pipeline, 'basic_pipeline.yaml')
+Compiler().compile(basic_pipeline, 'basic_pipeline.yaml')
